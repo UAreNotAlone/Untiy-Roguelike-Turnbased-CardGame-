fa2729b734d566436fd84dfc7549e34b388fc611
zhangzibo <3277718505@qq.com>
2022-11-25 23:12:30 +0800

    log

---
+++ Assets/Scripts/MapManager.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class MapManager : MonoBehaviour
+{
+
+    private static MapManager _instance;
+
+
+    public OverlayTile OverlayTilePrefab;
+    public GameObject overlayContainer;
+    public Dictionary<Vector2Int, OverlayTile> map; //  What for ?? -> Stores the upper layer of the tile
+
+    public static MapManager Instance
+    {
+        get { return _instance; }
+    }
+
+    private void Awake()
+    {
+        //  Make sure there is only one instance in a game scene.
+        if (_instance != null && _instance != this)
+        {
+            Destroy(this.gameObject);
+        }
+        else
+        {
+            _instance = this;
+        }
+    }
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+        //  To activate all the cell with a overlay tile.
+        var tileMap = gameObject.GetComponentInChildren<Tilemap>();
+        Debug.Log(tileMap.name);
+        BoundsInt bounds = tileMap.cellBounds;
+        tileMap.CompressBounds();
+
+        map = new Dictionary<Vector2Int, OverlayTile>();
+        
+        
+        //  -Looping all the cells
+        for (int z = bounds.max.z; z >= bounds.min.z; z--)
+        {
+            for (int y = bounds.max.y; y > bounds.min.y; y--)
+            {
+                for (int x = bounds.max.x; x > bounds.min.x; x--)
+                {
+                    var tileLocation = new Vector3Int(x, y, z);
+                    var tileKey = new Vector2Int(x, y);
+                    if (tileMap.HasTile(tileLocation) && !map.ContainsKey(tileKey))
+                    {
+                        //Debug.Log(tileLocation);
+                        var overlayTile = Instantiate(OverlayTilePrefab, overlayContainer.transform);
+                        var cellWorldPosition = tileMap.GetCellCenterWorld(tileLocation);
+                        //  Place it higher then the cell.
+                        overlayTile.transform.position = new Vector3(cellWorldPosition.x, cellWorldPosition.y,
+                            cellWorldPosition.z + 1);
+                       //Debug.Log(cellWorldPosition);
+                        //  Align the sorting order.
+                        overlayTile.GetComponent<SpriteRenderer>().sortingOrder =
+                            tileMap.GetComponent<TilemapRenderer>().sortingOrder + 1;
+                        overlayTile.gridLocation = tileLocation;
+                        map.Add(tileKey, overlayTile);  //  Only record the upper layer tiles in the map.
+                    }
+
+                }
+            }
+        }
+        
+        
+        
+        
+        
+    }
+
+
+}
---
+++ Assets/Scripts/PathFinder.cs
@@ -0,0 +1,138 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class PathFinder : MonoBehaviour
+{
+   public List<OverlayTile> FindPath(OverlayTile start, OverlayTile End)
+   {
+      // Can we use a PriorityQueue instead?
+      // Init
+      List<OverlayTile> openList = new List<OverlayTile>();
+      List<OverlayTile> closeList = new List<OverlayTile>();
+      openList.Add(start);
+      
+      Debug.Log("[FindPath]: PathFinder starts to work.");
+      // Find the shortest path
+      while (openList.Count > 0)
+      {
+         Debug.Log("[FindPath]: PathFinder in the while loop.");
+         // Get the node with the smallest F cost.
+         OverlayTile currentOverlayTile = openList.OrderBy(x => x.pathf_F).First();
+         openList.Remove(currentOverlayTile);
+         closeList.Add(currentOverlayTile);
+         if (currentOverlayTile == End)
+         {
+            // final path
+            Debug.Log("[FindPath]: PathFinder starts to return the final path.");
+            return GetFinishedList(start, End);
+         }
+
+         List<OverlayTile> neighbourTiles = GetNeighbourTiles(currentOverlayTile);
+         foreach (var neighbour in neighbourTiles)
+         {
+            
+            if (neighbour.isBlocked || closeList.Contains(neighbour) || Mathf.Abs(neighbour.gridLocation.z - currentOverlayTile.gridLocation.z) > 1)
+            {
+               continue;
+            }
+            
+            // Cal the manhattan distance
+            Debug.Log("[FindPath]: PathFinder starts to cal all the neighbours.");
+            neighbour.pathf_G = GetManhattenDistance(start, neighbour);
+            neighbour.pathf_H = GetManhattenDistance(End, neighbour);
+            neighbour.previousTile = currentOverlayTile;
+
+            if (!openList.Contains(neighbour))
+            {
+               Debug.Log("[FindPath]: PathFinder starts to add all the neighbours.");
+               openList.Add(neighbour);
+            }
+         }
+
+      }
+      // Empty if it is invalid
+      Debug.Log("[FindPath]: PathFinder return the empty path.");
+      return new List<OverlayTile>();
+   }
+
+   public List<OverlayTile> GetFinishedList(OverlayTile Start, OverlayTile End)
+   {
+      List<OverlayTile> finishedList = new List<OverlayTile>();
+      OverlayTile current = End;
+      while (current != Start)
+      {
+         finishedList.Add(current);
+         current = current.previousTile;
+         Debug.Log(current.gridLocation);
+      }
+
+      finishedList.Reverse();
+      return finishedList;
+   }
+
+   public int GetManhattenDistance(OverlayTile tile1, OverlayTile tile2)
+   {
+      return Mathf.Abs(tile1.gridLocation.x - tile2.gridLocation.x) +
+             Mathf.Abs(tile1.gridLocation.y - tile2.gridLocation.y);
+   }
+
+   public List<OverlayTile> GetNeighbourTiles(OverlayTile current)
+   {
+      // Get all the tile in our map
+      // -> map is initialized when the scene is loaded
+      Dictionary<Vector2Int, OverlayTile> map = MapManager.Instance.map;
+      List<OverlayTile> neighbours = new List<OverlayTile>();
+      // Get the tile's location in the grid -> easier to loop all the neighbours
+      
+      // [TODO]: change the logic here
+      // y+1 -> top neigbour
+      Vector2Int locationToCheck = new Vector2Int(
+         current.gridLocation.x, 
+         current.gridLocation.y + 1);
+      if (map.ContainsKey(locationToCheck))
+      {
+         neighbours.Add(map[locationToCheck]);
+      }
+      
+      
+      // bottom neigbour
+      locationToCheck = new Vector2Int(
+         current.gridLocation.x, 
+         current.gridLocation.y - 1);
+      if (map.ContainsKey(locationToCheck))
+      {
+         neighbours.Add(map[locationToCheck]);
+      }
+      
+      // right neigbour
+      locationToCheck = new Vector2Int(
+         current.gridLocation.x + 1, 
+         current.gridLocation.y);
+      if (map.ContainsKey(locationToCheck))
+      {
+         neighbours.Add(map[locationToCheck]);
+      }
+      
+      
+      // left neigbour
+      locationToCheck = new Vector2Int(
+         current.gridLocation.x - 1, 
+         current.gridLocation.y);
+      if (map.ContainsKey(locationToCheck))
+      {
+         neighbours.Add(map[locationToCheck]);
+      }
+      
+      
+      return neighbours;
+
+   }
+   
+   
+   
+   
+   
+   
+}
